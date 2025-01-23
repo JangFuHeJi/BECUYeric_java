01a175733ac9b12824352e24ccbb4c9839116a64
oryun <gaochenix@sina.com>
2025-01-23 15:40:22 +0800

    Initial commit

--- src/main/java/com/eric/BECUYeric.java
+++ src/main/java/com/eric/BECUYeric.java
@@ -11,6 +11,7 @@ public class BECUYeric implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final String ModID = "becuyeric";
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
